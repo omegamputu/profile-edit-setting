8062d02acb6a5f67da39cfc58d09922937cab5a9
Omega Mputu <kutekemeni93@gmail.com>
2021-12-17 13:01:21 +0100

    Express configuration issue fixed

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = express();
 
 const port = 5000;
 
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
     res.sendFile('index.html', {root: __dirname});
 });
